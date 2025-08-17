0924b0839fb5c297e10c411137cf5b2f9457b624
cono6783 <86134894+cono6783@users.noreply.github.com>
2025-08-16 23:32:32 -0500

    lots of prod development finally making it to git

--- main.py
+++ main.py
@@ -1,21 +1,72 @@
 import subprocess
 import json
 import os
+import eyed3
+import yt_dlp
 
-for file in os.listdir("downloaded"):
-    if not file.endswith("mp3"): continue
-    name = file.split(".")[0]
-    with open(f'downloaded/{name}.info.json', "r") as jsonfile:
-        jsondata = json.load(jsonfile)
-    print(f'Artist(s): {jsondata["artists"]}')
-    print(f'Album: {jsondata["album"]}')
-    print(f'Track: {jsondata["track"]}')
-    print()
-
-    os.setxattr(f"downloaded/{file}", "artist", jsondata["artists"]) #This exists in linux but not windows :)
-    os.setxattr(f"downloaded/{file}", 'album', jsondata['album'])
-    os.setxattr(f"downloaded/{file}", 'title', jsondata['track'])
+def main():
 
 
 
+	ytdl_options = {'allow_playlist_files': False,
+	'extract_flat': 'discard_in_playlist',
+	'final_ext': 'mp3',
+	'format': 'ba[acodec^=mp3]/ba/b',
+	'fragment_retries': 10,
+	'ignoreerrors': 'only_download',
+	'noplaylist': True,
+	'outtmpl': {'default': 'downloaded/%(id)s.%(ext)s', 'pl_thumbnail': ''},
+	'postprocessors': [{'key': 'FFmpegExtractAudio',
+		'nopostoverwrites': False,
+		'preferredcodec': 'mp3',
+		'preferredquality': '5'},
+		{'already_have_thumbnail': False, 'key': 'EmbedThumbnail'},
+		{'key': 'FFmpegConcat',
+		'only_multi_video': True,
+		'when': 'playlist'}],
+	'retries': 10,
+	'writeinfojson': True,
+	'writethumbnail': True}
+	
+	with open("download.txt", "r") as urlfile:
+		URLS = urlfile.readlines()
+	with yt_dlp.YoutubeDL(ytdl_options) as ytdl:
+		ytdl.download(URLS)
 
+	for file in os.listdir("downloaded"):
+		if file.endswith("mp3"): continue
+		name = file.split(".")[0]
+		with open(f"downloaded/{name}.info.json", "r") as jsonfile:
+			jsondata = json.load(jsonfile)
+		
+		
+		
+		print()
+		
+		audioFile = eyed3.load(f"downloaded/{name}.mp3")
+		audioFile.tag.clear()
+		if 'artists' in jsondata:
+			print(f'Artist(s): {jsondata["artists"]}')
+			audioFile.tag.artist = gen_artist_list(jsondata["artists"])
+		if 'album' in jsondata:
+			print(f'Album: {jsondata["album"]}')
+			audioFile.tag.album = jsondata['album']
+		if 'title' in jsondata:
+			print(f'Title: {jsondata["title"]}')
+			audioFile.tag.title = jsondata['title']
+
+		audioFile.tag.save(version=eyed3.id3.ID3_V2_3)
+		os.remove(f"downloaded/{name}.info.json")
+
+
+
+def gen_artist_list(artistList):
+	output = ""
+	for artist in artistList:
+		output += artist + "/"
+	return output.removesuffix("/")
+
+
+
+if __name__ == "__main__":
+	main()
