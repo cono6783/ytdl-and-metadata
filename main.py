9dc7d26647fd8a14e32e6c48a3b0c818b76db18e
cono6783 <86134894+cono6783@users.noreply.github.com>
2025-08-20 16:41:20 -0500

    Added functioning album downloading

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import subprocess
 import json
 import os
 import eyed3
+from eyed3.id3.frames import ImageFrame
 import yt_dlp
 import re
+import requests
 from re import Pattern
 
 from songdata import SongData
+import songdata as sd
+import playlistparser as playlist
 
 
 
@@ -14,6 +18,8 @@ from songdata import SongData
 
 
 def main():
+
+
 
 
 
@@ -36,12 +42,45 @@ def main():
 	'retries': 10,
 	'writeinfojson': True,
 	'writethumbnail': True}
+
+
+	
 	
 	with open("download.txt", "r") as urlfile:
 		URLS = urlfile.readlines()
+
+	for url in URLS:
+		if playlist.isPlaylist(url):
+			print("Autodetected as playlist")
+			ytdl_options["writeinfojson"] = False
+			ytdl_options["writethumbnail"] = False
+			playlistData = playlist.parsePlaylist(url)
+			URLS.remove(url)
+
+			with yt_dlp.YoutubeDL(ytdl_options) as ytdl:
+				ytdl.download(playlistData.urls)
+
+			applyMetadataWithPlaylistData(playlistData)
+		else:
+			ytdl_options["writeinfojson"] = True
+			ytdl_options["writethumbnail"] = True
+			
+			with yt_dlp.YoutubeDL(ytdl_options) as ytdl:
+				ytdl.download(url)
+
+			applyMetadataWithJSON()
+			
+			
+
+
 	with yt_dlp.YoutubeDL(ytdl_options) as ytdl:
 		ytdl.download(URLS)
 
+	
+
+
+
+def applyMetadataWithJSON():
 	for file in os.listdir("downloaded"):
 		if file.endswith("mp3"): continue
 		name = file.split(".")[0]
@@ -52,7 +91,8 @@ def main():
 		
 		print()
 		#First parse the title
-		songData = SongData(extract(jsondata["title"]))
+		songData = extract(jsondata["title"])
+		if songData == None: return
 
 		#Overwrite b/c if i had to parse the title then the data is not going to be in the json
 		songData.overwrite(jsondata)
@@ -62,6 +102,49 @@ def main():
 		songData.writeDataToFile(f"downloaded/{name}.mp3")
 
 		os.remove(f"downloaded/{name}.info.json")
+
+def applyMetadataWithPlaylistData(playlistData : playlist.PlaylistData):
+
+	#Download albumImage first
+	response = requests.get(playlistData.albumImage, stream=True) # Not entirely sure if a stream is required
+	if response.status_code == 200:
+		with open(f"downloaded/{playlistData.albumName}.jpg", "wb") as imgFile:
+			for chunk in response.iter_content(chunk_size=8192):
+				imgFile.write(chunk)
+		print(f"{playlistData.albumName}.jpg successfully downloaded")
+	else:
+		print(f"Failed to download image with err code {response.status_code}")
+
+
+
+	for file in os.listdir("downloaded"):
+		if playlistData.hasDataFor(file):
+			songData = playlistData.generateSongData(file.removesuffix('.mp3'))
+
+			print(songData)
+
+			songData.writeDataToFile(f"downloaded/{file}")
+
+			audioFile = eyed3.load(f"downloaded/{file}")
+			if audioFile == None:
+				print(f"Writing Data failed: Could not find {file}")
+				continue
+
+			with open(f"downloaded/{playlistData.albumName}.jpg", "rb") as imgFile:
+				audioFile.tag.images.set(
+					ImageFrame.FRONT_COVER,
+					imgFile.read(),
+					"image/jpeg"
+				)
+			
+			audioFile.tag.save()
+
+	os.remove(f"downloaded/{playlistData.albumName}.jpg")
+
+			
+
+			
+
 
 
 
@@ -77,7 +160,7 @@ def extract(s):
     for pattern in patterns:
         match = pattern.match(s)
         if match != None:
-            return SongData(match)
+            return sd.fromMatch(match)
 
     print(f"No match found for {s}")
     return None
